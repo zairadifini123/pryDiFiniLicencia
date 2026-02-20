d8a931f54daf904884470d27dfb20ba14b412bb4
Zaira Di Fini <zairadifini123@gmail.com>
2026-02-20 17:00:27 -0300

    .

--- pryDiFiniLicencia/Form1.Designer.cs
+++ pryDiFiniLicencia/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(586, 450);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "frmLicencia";
             StartPosition = FormStartPosition.CenterScreen;
